a30b3c43791128063719b1c0a7c7dacf223f3dbe
nontajid <nontajid@gmail.com>
2018-06-30 03:00:30 +0700

    feature - retrive a header

--- index.js
+++ index.js
@@ -3,16 +3,52 @@
 const http = require('http');
 const fs = require('fs');
 
-const CircularJSON = require('circular-json'); //dev
 const config = {
     port: '18001',
     log: './log.txt'
 };
 
+let handdlePostRequest = function() {
+    return new Promise((resolve,reject) => {
+        const data = {
+            headers: this.headers,
+            body: this.body
+        }
+        fs.appendFile(
+            config.log, 
+            JSON.stringify(data),
+            (err) => { 
+                if ( err ) {
+                    reject(err)
+                } else {
+                    resolve({data: 'hey'}); 
+                }
+            } 
+        );
+    });
+}
+
 const server = http.createServer(function (req, res) {
-    fs.appendFile(config.log, CircularJSON.stringify(req),(err) => { console.log(err) });
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('OK');
+    this.body = [];
+    this.headers = req.headers;
+    this.req = req;
+
+    req.on('data', (chunk) => {
+        this.body.push(chunk);
+      }).on('end', () => {
+        this.body = JSON.parse(Buffer.concat(this.body).toString());
+
+        // Handle Post Request
+        if ( this.req.method == 'POST' ) { 
+            handdlePostRequest = handdlePostRequest.bind(this);
+
+            handdlePostRequest()
+            .then( (data) => {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(data));        
+            });
+        }
+    });
 });
 
 server.listen(config.port);
